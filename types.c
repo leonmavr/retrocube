9df3bddbbf1b0073cd7273cc30ebd6971f8e1b22
leonmavr <0xleo.git@gmail.com>
2022-11-22 13:09:18 +0100

    Add ray-plane intersection

--- types.c
+++ types.c
@@ -6,6 +6,7 @@
 #define HALF_SQRT_TWO 0.7071065 
 
 
+
 Vec3i* vec3iNew(int x, int y, int z) {
     Vec3i* new = malloc(sizeof(Vec3i));
     new->x = x;
@@ -23,6 +24,12 @@ Vec3f* vec3fNew(int x, int y, int z) {
 }
 
 void vec3iCopy(Vec3i* dest, Vec3i* src) {
+    dest->x = src->x;
+    dest->y = src->y;
+    dest->z = src->z;
+}
+
+void vec3fCopy(Vec3f* dest, Vec3f* src) {
     dest->x = src->x;
     dest->y = src->y;
     dest->z = src->z;
@@ -62,4 +69,40 @@ Ray* rayNew(int x, int y, int z) {
 
 void raySetColor(Ray* ray, char color) {
     ray->color = color;
+}
+
+
+static Vec3f vec3fCrossProd(Vec3i* v1, Vec3i* v2) {
+    Vec3f ret;
+    ret.x = v1->y*v2->z - v1->z*v2->y;  
+    ret.y = v1->z*v2->x - v1->x*v2->z;
+    ret.z = v1->x*v2->y - v1->y*v1->x; 
+    return ret;
+}
+
+static float vec3fDotprod(Vec3f* v1, Vec3f* v2) {
+    return v1->x*v2->x + v1->y*v2->y + v1->z*v2->z;
+}
+
+Vec3i rayPlaneIntersection(Ray* ray, Vec3i* p0, Vec3i* p1, Vec3i* p2) {
+    // find the equation of the plane defined by p0, p1, p2
+    Vec3i p1p0 = (Vec3i) {p1->x - p0->x, p1->y - p0->y, p1->z - p0->z};
+    Vec3i p2p0 = (Vec3i) {p2->x - p0->x, p2->y - p0->y, p2->z - p0->z};
+    // normal vector to the plane 
+    Vec3f n = vec3fCrossProd(&p1p0, &p2p0);
+    // see https://9to5science.com/line-and-plane-intersection-in-3d for the intersection
+    // ray's origin
+    Vec3f a = (Vec3f) {ray->orig->x, ray->orig->y, ray->orig->z};
+    // ray's direction
+    Vec3f b = (Vec3f) {ray->dir->x, ray->dir->y, ray->dir->z};
+    Vec3f ba = (Vec3f) {b.x - a.x, b.y - a.y, b.z - a.z};
+    Vec3f fp0 = (Vec3f) {p0->x, p0->y, p0->z};
+    // intersection of parametric ray: r(t) = a + t(b-a)
+    float t = (vec3fDotprod(&n, &fp0) - vec3fDotprod(&n, &a)) /
+        (vec3fDotprod(&n, &b) - vec3fDotprod(&n, &a)); 
+    // return the point r(t) on the ray for the interesection t: r(t) = a + t*(b-a)
+    Vec3i ret = (Vec3i) {a.x + t*(b.x - a.x), 
+                         a.y + t*(b.y - a.y), 
+                         a.z + t*(b.z - a.z)};
+    return ret;
 }
--- types.h
+++ types.h
@@ -27,10 +27,12 @@ Vec3i* vec3iNew(int x, int y, int z);
 Vec3f* vec3fNew(int x, int y, int z);
 void vec3fMakeUnit(Vec3f* vec);
 void vec3iCopy(Vec3i* dest, Vec3i* src);
+void vec3fCopy(Vec3f* dest, Vec3f* src);
 Cube* cubeNew(int cx, int cy, int cz, int size);
 // the pixel in the screen where the ray points to
 Ray* rayNew(int x, int y, int z);
 void raySetColor(Ray* ray, char color);
+Vec3i rayPlaneIntersection(Ray* ray, Vec3i* p0, Vec3i* p1, Vec3i* p2);
 
 
 #endif /* OBJECTS_H */
