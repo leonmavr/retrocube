b384e04a2db715c1ed028d2d2d993c858d67eeae
leonmavr <0xleo.git@gmail.com>
2023-01-23 22:36:44 +0100

    Use random rotations by default, lower max fps to reduce flicker

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include "vector.h"
 #include "objects.h"
 #include <ncurses.h> // clear
-#include <time.h> // nanosleep 
 #include <math.h> // sin, cos
 #include <stdlib.h> // atof, atoi, random
 #include <unistd.h> // for usleep
@@ -10,7 +9,6 @@
 #include <stdbool.h>
 #include <string.h> // strcmp
 #include <limits.h> // UINT_MAX
-#include <time.h> // time
 
 
 //// default command line arguments
@@ -19,8 +17,8 @@ static float g_rot_speed_x = 0.7;
 static float g_rot_speed_y = 0.4;
 static float g_rot_speed_z = 0.6;
 // maximum fps at which to render the cube
-static unsigned g_fps = 40;
-static bool g_use_random_rotation = false;
+static unsigned g_fps = 20;
+static bool g_use_random_rotation = true;
 // centre (x, y, z) of the cube
 static int g_cx = 0;
 static int g_cy = 0;
@@ -33,25 +31,27 @@ static unsigned g_max_iterations = UINT_MAX;
 
 int main(int argc, char** argv) {
     int i = 0;
-    float random_bias_x, random_bias_y, random_bias_z;
+    // initialise pseudo randomness generator for random rotations
+    srand(time(NULL));
+    // from 0 to 1.25
+    float random_bias_x = 1.25*rand() / (double)RAND_MAX;
+    float random_bias_y = 1.25*rand() / (double)RAND_MAX;
+    float random_bias_z = 1.25*rand() / (double)RAND_MAX;
     // parse command line arguments - if followed by an argument, e.g. -sx 0.9, increment `i`
     while (++i < argc) {
         if ((strcmp(argv[i], "--speedx") == 0) || (strcmp(argv[i], "-sx") == 0)) {
             g_rot_speed_x = atof(argv[++i]);
+            g_use_random_rotation = false;
         } else if ((strcmp(argv[i], "--speedy") == 0) || (strcmp(argv[i], "-sy") == 0)) {
             g_rot_speed_y = atof(argv[++i]);
+            g_use_random_rotation = false;
         } else if ((strcmp(argv[i], "--speedz") == 0) || (strcmp(argv[i], "-sz") == 0)) {
             g_rot_speed_z = atof(argv[++i]);
+            g_use_random_rotation = false;
         } else if ((strcmp(argv[i], "--fps") == 0) || (strcmp(argv[i], "-f") == 0)) {
             g_fps = atoi(argv[++i]);
         } else if ((strcmp(argv[i], "--random") == 0) || (strcmp(argv[i], "-r") == 0)) {
             g_use_random_rotation = true;
-            // initialise pseudo randomness generator
-            srand(time(NULL));
-            // from 0 to 1.25
-            random_bias_x = 1.25*rand() / (double)RAND_MAX;
-            random_bias_y = 1.25*rand() / (double)RAND_MAX;
-            random_bias_z = 1.25*rand() / (double)RAND_MAX;
         } else if ((strcmp(argv[i], "--cx") == 0) || (strcmp(argv[i], "-cx") == 0)) {
             g_cx = atoi(argv[++i]);
         } else if ((strcmp(argv[i], "--cy") == 0) || (strcmp(argv[i], "-cy") == 0)) {
@@ -73,12 +73,12 @@ int main(int argc, char** argv) {
 
     for (size_t t = 0; t < g_max_iterations; ++t) {
         clear();
-        if (!g_use_random_rotation)
-            obj_cube_rotate(cube, g_rot_speed_x/20*t, g_rot_speed_y/20*t, g_rot_speed_z/20*t);
+        if (g_use_random_rotation)
+            obj_cube_rotate(cube, 4*sin((0.5 + random_bias_x)*sin(0.0025*t) + 2*random_bias_x),
+                                  4*sin((0.5 + random_bias_y)*0.0025*t + 2*random_bias_y),
+                                  4*sin((0.5 + random_bias_z)*0.0025*t + 2*random_bias_z));
         else
-            obj_cube_rotate(cube, 4*sin((0.5 + random_bias_x)*sin(0.0025*t) + random_bias_x),
-                                  4*sin((0.5 + random_bias_y)*0.0025*t + random_bias_y),
-                                  4*sin((0.5 + random_bias_z)*0.0025*t + random_bias_z));
+            obj_cube_rotate(cube, g_rot_speed_x/20*t, g_rot_speed_y/20*t, g_rot_speed_z/20*t);
         draw_cube(cube);
         //nanosleep((const struct timespec[]) {{0, (int)(1.0 / g_fps * 1e9)}}, NULL);
         usleep((int)(1.0 / g_fps * 1e6));
