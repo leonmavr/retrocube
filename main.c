67b2173d0c47fd232fbc69dd78c741f9ff34738d
0xleo <0xleo.git@gmail.com>
2023-04-04 12:20:43 +0300

    Try calling obj_mesh_from_file

--- main.c
+++ main.c
@@ -87,7 +87,11 @@ int main(int argc, char** argv) {
     screen_init();
     render_init();
 
-    mesh_t* shape = obj_mesh_new(g_cx, g_cy, g_cz, g_cube_size, 1.5*g_cube_size, 1.8*g_cube_size, TYPE_BLOCK);
+#if 0
+    mesh_t* shape = obj_mesh_new(g_cx, g_cy, g_cz, g_cube_size, 1.5*g_cube_size, 1.8*g_cube_size, TYPE_RHOMBUS);
+#else
+    mesh_t* shape = obj_mesh_from_file("./mesh_files/cube.sci", g_cx, g_cy, g_cz, g_cube_size, 1.5*g_cube_size, 1.8*g_cube_size);
+#endif
     // spinning parameters in case random rotation was selected
 #ifndef _WIN32
     const float random_rot_speed_x = 0.002, random_rot_speed_y = 0.002, random_rot_speed_z = 0.002;
